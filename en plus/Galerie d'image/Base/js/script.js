f27243041a0e516735f5da90508447c58f4428c8
Audrey <danataelia@gmail.com>
2023-06-06 17:46:24 +0200

    Ajout de tp en plus

---
+++ en plus/Galerie d'image/Base/js/script.js
@@ -0,0 +1,11 @@
+// Pour cet exercice il vous faudra obligatoirement créer 2 fonctions :
+
+/*
+
+- Une fonction displayImage qui permettra l'ouverture d'un l'overlay avec l'image et la croix
+- Une fonction removeImage qui permettra la suppression de l'overlay
+Vous pouvez vous aider de l'exercice sur l'overlay qui utilise le même principe
+
+Les datasets seront également nécessaires pour pouvoir identifier l'image à faire apparaître dans l'overlay.
+
+*/
